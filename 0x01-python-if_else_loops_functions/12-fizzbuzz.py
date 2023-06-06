135832d51b16ece8788f21dc18d1239757512480
yonessgt <youness.merzak123@gmail.com>
2023-06-06 18:15:15 +0000

    12-fizzbuzz.py python script

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+       # if i == 100:
+            #print("Buzz ")
+        f = int(i % 3)
+        b = int(i % 5)
+        if f == 0 and b != 0:
+            print("Fizz ", end="")
+        if b == 0 and f != 0:
+            print("Buzz ", end="")
+        if f == 0 and b == 0:
+            print("FizzBuzz ", end="")
+        if f != 0 and b != 0:
+            print("{} ".format(i), end="")
