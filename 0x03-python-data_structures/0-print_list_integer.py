e08a62400a4a6b3e27c9a1bcfdddf274d9d223ce
yonessgt <youness.merzak123@gmail.com>
2023-06-10 15:36:42 +0000

    0-print_list_integer.py python function

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def print_list_integer(my_list=[]):
-        for i in range(len(my_list)):
-            print("{}".format(my_list[i]))
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
