f88bd30babd2faa3ca80a209459e5e0f78b9b9a3
yonessgt <youness.merzak123@gmail.com>
2023-06-11 13:47:10 +0000

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,6 +6,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         new = tuple_a[0] + tuple_b[0], tuple_b[1]
     if len(tuple_a) >= 2 and len(tuple_b) == 1:
         new = tuple_a[0] + tuple_b[0], tuple_a[1]
+    if len(tuple_a) == 1 or len(tuple_b) == 1:
+         new = tuple_a[0] + tuple_b[0], 0
     if len(tuple_a) == 0 or len(tuple_b) == 0:
         if len(tuple_a) == 0:
             new = tuple_b
