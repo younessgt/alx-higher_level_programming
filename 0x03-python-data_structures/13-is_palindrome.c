acc2b5a1f243ebbfcff600ec905bc66ff805db9d
yonessgt <youness.merzak123@gmail.com>
2023-06-12 15:48:54 +0000

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+listint_t *rev(listint_t **head)
+{
+	listint_t *temp, *current, *prev = NULL;
+	temp = *head;
+
+	if (*head == NULL)
+		return (NULL);
+	while (temp != NULL)
+	{
+		current = temp->next;
+		temp->next = prev;
+		prev = temp;
+		temp = current;
+	}
+	return (prev);
+}
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *copy;
+	copy = rev(head);
+	if (copy == NULL)
+		return (1);
+	while(*head != NULL)
+	{
+		if (copy->n == (*head)->n)
+		{
+			*head = (*head)->next;
+			copy = copy->next;
+		}
+		else
+			return (0);
+	}
+	return (1);
+}
