7185fbaf4d5e29f5285ed6e90d204a28f0603d45
younessgt <youness.merzak123@gmail>
2023-06-12 17:02:45 -0700

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,15 @@
+#include "Python.h"
+/**
+ * print_python_list_info - function that prints some basic
+ * info about Python lists.
+ * @p: pointer to structure.
+ */
+void print_python_list_info(PyObject *p)
+{
+	int i;
+
+	printf("[*] Size of the Python List = %d\n", Py_SIZE(p));
+	printf("[*] Allocated = %d\n", ((PyListObject *)(p))->allocated);
+	for (i = 0; i < Py_SIZE(p); i++)
+		printf("Element %d: %s\n", i, Py_TYPE((PyList_GetItem(p, i))->tp_name);
+}
