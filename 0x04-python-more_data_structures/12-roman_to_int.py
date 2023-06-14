5d044e9b3a24f19b9ca6e93e3ddb2cdbe84ca71f
yonessgt <youness.merzak123@gmail.com>
2023-06-14 17:59:23 +0000

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dict_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dict_roman = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+            }
     summ = 0
     prev_val = 0
     if not isinstance(roman_string, str) or roman_string is None:
@@ -8,7 +10,7 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         if roman_string[i] not in 'IVXLCDM':
             return 0
-        val= dict_roman[roman_string[i]]
+        val = dict_roman[roman_string[i]]
         if val > prev_val:
             summ += val - 2 * prev_val
         else:
