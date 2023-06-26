d00a01a2c89fb6a0cd4c1bc102ddf4293e095ae6
younessgt <youness.merzak123@gmail.com>
2023-06-26 12:03:23 +0100

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    for i in my_list:
+        j += 1
+    for k in range(x):
+        try:
+            print(my_list[k], end='')
+        except:
+            break
+    print()
+    if x <= j:
+        return x
+    else:
+        return j
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This directory is for exceptions in python 
