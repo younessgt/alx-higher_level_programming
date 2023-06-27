3a3d22a06042c4686a7ae51a06baea2f768f655a
younessgt <youness.merzak123@gmail.com>
2023-06-27 14:57:03 +0100

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" creating a class named Square that defines a square"""
+
+
+class Square:
+    """ Square class is now created"""
+    def __init__(self, size):
+        """ Initializing the data."""
+        self.__size = size
