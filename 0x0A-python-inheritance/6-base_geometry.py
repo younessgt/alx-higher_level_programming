a77ad470da53b4b86e04b67c7cfdd99ae85a5401
younessgt <youness.merzak123@gmail.com>
2023-07-10 18:32:12 +0100

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module containing a class called BaseGeometry """
+
+
+class BaseGeometry:
+    """ the class is created"""
+    def __init__(self):
+        """ constructor """
+        pass
+
+    def area(self):
+        """ function not implemented yet"""
+        raise Exception("area() is not implemented")
