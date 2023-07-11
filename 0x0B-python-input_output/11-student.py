2b7d94df327db2d961071b43fd399d03f3eab7b3
younessgt <youness.merzak123@gmail.com>
2023-07-11 22:56:32 +0100

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Module containing a class called Student"""
+
+
+class Student:
+    """ Student class is created"""
+
+    def __init__(self, first_name, last_name, age):
+        """ constructor """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attr=None):
+        """ retreving a dictionary representation
+        of a Student instance"""
+
+        if attr is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for attri in attr:
+                if attri in self.__dict__:
+                    new_dict.update({attri: self.__dict__[attri]})
+            return new_dict
+
+    def reload_from_json(self, json):
+        """
+        function that replace the self value attribute with json
+        value
+        """
+
+        dict_2 = self.__dict__
+        for key in dict_2:
+            if key in json:
+                self.__dict__[key] = json[key]
