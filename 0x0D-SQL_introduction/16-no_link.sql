d2575464f13be76ade27ac180f7b36bb7d7b12f8
younessgt <youness.merzak123@gmail.com>
2023-08-15 18:18:44 +0100

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- Script that lists all records of the table second_table
 -- Rows without a name value are not listed
-SELECT score, name FROM second_table WHERE name IS NOT NULL AND > LEN(name) > 0 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND  LENGTH(name) > 0 ORDER BY score DESC;
