d40e95be279061445e69279ed267352ee5203490
younessgt <youness.merzak123@gmail.com>
2023-08-16 17:00:51 +0100

    7-cities.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- script that creates the database hbtn_0d_usa and the table states
 -- id INT unique, auto generated, can’t be null and is a primary key
 -- name VARCHAR(256) can’t be null
+-- with PRIMARY KEY -> NOT NULL + UNIQUNESS
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
