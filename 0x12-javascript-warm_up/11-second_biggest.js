7959caf250254ff539b9245c44635a0c2a8aefe0
younessgt <youness.merzak123@gmail.com>
2023-09-11 18:48:57 +0100

    12-object.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const arg = process.argv;
+/* sort the number in ascending order */
 const numArr = arg.slice(2).sort((a, b) => a - b);
 const len = numArr.length;
 if (arg.length <= 3) {
