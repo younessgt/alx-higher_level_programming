c6d5be12b6f7a94aeb95824d50bd69597e66f708
younessgt <youness.merzak123@gmail.com>
2023-09-11 15:08:54 +0100

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const arg = process.argv;
-if (arg.length > 2) {
+if (arg.length === 3) {
   console.log('Argument found');
+} else if (arg.length > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
