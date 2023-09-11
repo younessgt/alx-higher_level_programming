c49d7ee8ded272421956bb383367a999538842b7
younessgt <youness.merzak123@gmail.com>
2023-09-11 18:35:42 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const arg = process.argv;
-const numArr = arg.slice(2).sort();
+const numArr = arg.slice(2).sort((a, b) => a - b);
 const len = numArr.length;
 if (arg.length <= 3) {
   console.log(0);
