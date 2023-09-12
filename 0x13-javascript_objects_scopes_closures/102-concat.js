1de173f71a933441b1576da0ab2c49ebb9a26698
younessgt <youness.merzak123@gmail.com>
2023-09-12 23:47:01 +0100

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,6 +4,6 @@ const arg = process.argv.slice(2);
 if (arg.length === 3) {
   const contFile1 = fs.readFileSync(arg[0], 'utf8');
   const contFile2 = fs.readFileSync(arg[1], 'utf8');
-  const contFile3 = contFile1 + '\n' + contFile2;
+  const contFile3 = contFile1 + '\n' + contFile2 + '\n';
   fs.writeFileSync(arg[2], contFile3);
 }
