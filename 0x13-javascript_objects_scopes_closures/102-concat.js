d6d895f6c8bdeeae1270e28da10475e79a958350
younessgt <youness.merzak123@gmail.com>
2023-09-12 23:54:30 +0100

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const arg = process.argv.slice(2);
-if (arg.length === 3) {
-  const contFile1 = fs.readFileSync(arg[0], 'utf8');
-  const contFile2 = fs.readFileSync(arg[1], 'utf8');
-  const contFile3 = contFile1 + '\n' + contFile2 + '\n';
-  fs.writeFileSync(arg[2], contFile3);
-}
+const contFile1 = fs.readFileSync(arg[0], 'utf8');
+const contFile2 = fs.readFileSync(arg[1], 'utf8');
+const contFile3 = contFile1 + contFile2;
+fs.writeFileSync(arg[2], contFile3);
