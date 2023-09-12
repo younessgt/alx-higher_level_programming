11854f83ec810f3fa65ad8c5a4848ec2b0cede06
younessgt <youness.merzak123@gmail.com>
2023-09-12 16:11:11 +0100

    7-occurrences.js

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (const elem of list) {
+    if (elem === searchElement) {
+      counter++;
+    }
+  }
+  return counter;
+};
