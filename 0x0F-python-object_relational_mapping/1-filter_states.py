cd9383f550e457c674eeb93d2a20b680d8aae346
yonessgt <youness.merzak123@gmail.com>
2023-09-15 17:24:07 +0000

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306
     )
     cur = db.cursor()  # cursor object
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
     res = cur.fetchall()  # retrieving a list of tuples
     for elem in res:
         print(elem)
