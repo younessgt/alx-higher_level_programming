9d8247f9b44dc6d833029d35dd636d1a134a2821
yonessgt <youness.merzak123@gmail.com>
2023-09-17 16:15:01 +0000

    100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,6 +5,7 @@ of a State and an instance Base = declarative_base() """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from relationship_city import City
 
 Base = declarative_base()
 
@@ -15,7 +16,7 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    # cities is an instance of of SQLAlchemy's relationship class 
+    # cities is an instance of of SQLAlchemy's relationship class
     # and behaves like a Python list
     cities = relationship("City", backref="state",
                           cascade="all, delete-orphan")
