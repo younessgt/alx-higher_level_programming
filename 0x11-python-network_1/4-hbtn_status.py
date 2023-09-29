eb84a5d317407cd39a955cd63ecadfa879bdc7bc
yonessgt <youness.merzak123@gmail.com>
2023-09-29 14:59:37 +0000

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,7 @@ import requests
 
 if __name__ == "__main__":
     res = requests.get('https://alx-intranet.hbtn.io/status')
+
     res_text = f"""Body response:
     \t- type: {type(res.text)}
     \t- content: {res.text}"""
