170d8e86000b24fa43455d60a322b0d3670079ac
yonessgt <youness.merzak123@gmail.com>
2023-10-24 14:38:52 +0000

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+
+const request = require('request');
+let count = 0;
+const url = process.argv[2];
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  if (!err && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const results = data.results;
+    for (const obj of results) {
+      const characters = obj.characters;
+      for (const character of characters) {
+        const url2 = 'https://swapi-api.alx-tools.com/api/people/18/';
+        if (character === url2) {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
