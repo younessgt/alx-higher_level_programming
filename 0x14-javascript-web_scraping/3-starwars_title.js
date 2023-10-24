a88bbc2d5cbd2da6ef8a077e302f40273a766369
yonessgt <youness.merzak123@gmail.com>
2023-10-24 13:53:44 +0000

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -9,6 +9,6 @@ request(url, (err, response, body) => {
   }
   if (!err && response.statusCode === 200) {
     const data = JSON.parse(body);
-    console.log(data['title']);
+    console.log(data.title);
   }
 });
