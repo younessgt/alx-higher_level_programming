faa08b6047862603b65331287d37f3968b94f852
yonessgt <youness.merzak123@gmail.com>
2023-10-24 11:58:36 +0000

    task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,12 +2,10 @@
 
 const fs = require('fs');
 
-const args = process.argv;
-
-fs.readFile(args[2], 'utf8', (err, content) => {
+fs.readFile(process.argv[2], 'utf8', (err, content) => {
   if (content) {
     console.log(content);
   } else {
-    console.log(err);
+    console.error(err);
   }
 });
