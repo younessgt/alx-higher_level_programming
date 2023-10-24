f730191c710cf4be10596801bae2729e93c01585
yonessgt <youness.merzak123@gmail.com>
2023-10-24 13:47:50 +0000

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  if (!err && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    console.log(data['title']);
+  }
+});
