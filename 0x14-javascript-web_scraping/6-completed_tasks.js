34a568e7e2ae3784a0c51e00a6001548fc16cd1e
yonessgt <youness.merzak123@gmail.com>
2023-10-24 16:19:48 +0000

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+  if (!err && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const result = {};
+    for (const obj of data) {
+      if (obj.completed) {
+        if (result[obj.userId]) {
+          result[obj.userId] += 1;
+        } else {
+          result[obj.userId] = 1;
+        }
+      }
+    }
+    console.log(result);
+  }
+});
